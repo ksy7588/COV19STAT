ded2983f00deefed3df5d016ecb47ebce71fe8bd
Anthony Kim <anthony092777@gmail.com>
2020-09-02 17:28:55 -0400

    Initial Commit

---
+++ COVSTAT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVSTAT
+//
+//  Created by Anthony Kim on 9/2/20.
+//  Copyright © 2020 Anthony Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
